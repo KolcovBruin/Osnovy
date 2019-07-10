093166f8423afc3506a972fe4956e17b1396814a
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-07-11 00:24:09 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <cstdlib>
+int br;
+int cntn2;
+int cntn1;
 int k;
 float rez1;
-float rez2;
+int rez2;
 int cntn=0;
 int x=1;
 int j;
@@ -33,6 +36,9 @@ int main()
    // sleep(3);
     //token
   td_json_client_send(client, "{\"@type\": \"checkAuthenticationBotToken\", \"token\":     \"862204240:AAHFzvfzbI7cqO3StzUoSoEdFp0f6imLXvE\"}");
+    td_json_client_send(client," {\"type\": \"sendMessage\",\"reply_markup_\": {\"replyMarkupShowKeyboard\": {\"keyboardButton\":{\"text\":\"knopka\", \"type\":\"keyboardButtonTypeText\"}}, \"one_time\":true}}");
+    std::cout << " {\"type\": \"sendMessage\",\"reply_markup_\": {\"replyMarkupShowKeyboard\": {\"keyboardButton\":{\"text\":\"knopka\", \"type\":\"keyboardButtonTypeText\"}}, \"one_time\":true}}}" << std::endl;
+    
     const double WAIT_TIMEOUT = 10.0;  // seconds
     while (true) {
         const char *sender = "{\"@type\":\"sendMessage\",\"chat_id\":111111111,\"input_message_content\":{\"@type\":\"inputMessageText\",\"text\":{\"@type\":\"formattedText\",\"text\":\"1\"}}}";
@@ -92,122 +98,61 @@ int main()
                 }
                 for(i=0;i>=0;i++)
                 {
-                    a=*(result+i);
+                    a=*(forresult+i);
                     if(a=='m')
                     {
-                        a=*(result+i+1);
+                        a=*(forresult+i+1);
                         if(a=='a')
                         {
                         for(i=i;i>=0;i++)
                         {
-                            a=*(result+i);
+                            a=*(forresult+i);
                             if(a==':')
                             {
                                 for(i=i;i>=0;i++)
                                 {
-                                    a=*(result+i);
+                                    a=*(forresult+i);
                                     if (a!=',')
                                     {
-                                        /*if (a=='+')
-                                        {
-                                            znak=a;
-                                            x=1;
-                                            sum+=b;
-                                            b=0;
-                                        }
-                                        if (a=='-')
-                                        {
-                                            znak=a;
-                                            x=1;
-                                            sum+=b;
-                                            b=0;
-                                         }    b*=x;
-                                         b+=(float)(a-'0');
-                                         x=10;*/
+                                        
                                         if (a=='*')
                                         {
-                                            for (j=i;j>0;j++)
+                                            b=0;
+                                            x=1;
+                                            j=i;
+                                            a=*(forresult+j-1);
+                                            while (a>='0'&&a<='9')
                                             {
-                                                
-                                                a=*(result+j+1);
+                                                 j--;
+                                                a=*(forresult+j-1);
+                                               
+                                            }
+                                            cntn1=i-j;
+                                            for(j=i-cntn1;j<i;j++)
+                                            {
+                                                a=*(forresult+j);
                                                 if (a>='0'&&a<='9')
                                                 {
                                                     b*=x;
                                                     b+=(float)(a-'0');
+                                                    std::cout <<b<< std::endl;
                                                     x=10;
                                                 }
                                                 else break;
                                             }
-                                            rez1+=b;
-                                            rez2=j-i;
-                                            
-                                            //delete stroka
-                                            /*
-                                             for (j=i;j>0;j++)
-                                             {
-                                             
-                                             a=*(result+j+1);
-                                             if (a>='0'&&a<='9')
-                                             {
-                                             b*=x;
-                                             b+=(float)(a-'0');
-                                             x=10;
-                                             }
-                                             else break;
-                                             }
-                                             
-                                             for (j=i;j>0;j--)
-                                             {
-                                             a=*(result+j-1);
-                                             if (a>='0'&&a<='9')
-                                             {
-                                             b*=x;
-                                             b+=(float)(a-'0');
-                                             x=10;
-                                             }
-                                             else break;
-                                             }
-                                             
-                                             if (j<=134)
-                                             {
-                                             memcpy(math+j,forsnd+j,sizeof(char));
-                                             }
-                                             else if ((j>134)&&(j<=134+cntn))
-                                             {
-                                             x/=10;
-                                             c1=(char)(sum/x)+'0';
-                                             memcpy(math+j,&c1,sizeof(char));
-                                             sum=sum-int(sum/x)*x;
-                                             }
-                                             else
-                                             {
-                                             memcpy(math+j,forsnd+(j-cntn+1),sizeof(char));
-                                             }
-                                             */
-                                           /* for (j=j;j<=j+rez2;j++)
-                                            {
-                                                memcpy(forresult+j,result+j+1,sizeof(char));
-                                            }*/
-                                            /*for (j=j;j>=j;j++)
-                                            {
-                                                memcpy(forresult+j,result+j+1,sizeof(char));
-                                            }*/
-                                            /*
-                                             else if ((j>134)&&(j<=134+cntn))
-                                             {
-                                             x/=10;
-                                             c1=(char)(sum/x)+'0';
-                                             memcpy(math+j,&c1,sizeof(char));
-                                             sum=sum-int(sum/x)*x;
-                                             }
-                                             */
+                                            rez1=b;
+                                            std::cout <<rez1<< std::endl;
+                                            rez2=cntn1;
                                             b=0;
                                             x=1;
-                                            for (j=i;j>0;j--)
+                                            
+                                            for (j=i;j>0;j++)
                                             {
-                                                a=*(result+j-1);
+                                                
+                                                a=*(forresult+j+1);
                                                 if (a>='0'&&a<='9')
                                                 {
+                                                    
                                                     b*=x;
                                                     b+=(float)(a-'0');
                                                     x=10;
@@ -215,40 +160,62 @@ int main()
                                                 else break;
                                             }
                                             rez1*=b;
-                                            rez2+=(i-j);
-                                            
+                                            std::cout <<rez1<< std::endl;
+                                            cntn2=j-i;
+                                            rez2+=cntn2;
                                             //delete stroka
                                             x=1;
                                              while ((int)(rez1/x)>=1)
                                              { x*=10;
                                                  cntn++;
                                              }
-                                            k=j+cntn;
-                                            for (j=j;j<k;j++)
+                                            k=j+cntn-rez2-1;
+                                            //j=i-cntn1;
+                                            for (j=i-cntn1-1;j<(i-cntn1+cntn)-1;j++)
                                             {
                                                  x/=10;
                                                 c1=(char)(rez1/x)+'0';
-                                                memcpy(forresult+j,&c1,sizeof(char));
+                                                std::cout <<c1<< std::endl;
+                                                memcpy(forresult+j+1,&c1,sizeof(char));
                                                 rez1=rez1-int(rez1/x)*x;
-                                               
+                                               std::cout <<forresult<< std::endl;
+                                                //sleep(1);
                                             }
-                                            for (j=j;j<=1000;j++)
+                                            for (j=j+1;j<=1000;j++)
                                             {
-                                                memcpy(forresult+j,result+j+1,sizeof(char));
+                                                memcpy(forresult+j,forresult+i+cntn2+1,sizeof(char));
+                                                i++;
+                                                std::cout <<forresult<< std::endl;
+                                                //sleep(2);
                                             }
                                             std::cout <<forresult<< std::endl;
-                                            /*znak=a;
-                                            x=1;
-                                            sum+=b;
-                                            b=0;*/
+                                           // sleep(2);
+                                            br=1;
+                                            i=580;
+                                           /* if (znak=='+')
+                                            {
+                                                sum+=b;
+                                                znak='0';
+                                                std::cout <<sum<< std::endl;
+                                                
+                                            }*/
                                         }
-                                        if (a=='/')
+                                        b=0;
+                                        x=1;
+                                        j=0;
+                                        rez2=0;
+                                        cntn=0;
+                                        cntn1=0;
+                                        cntn2=0;
+                                        rez1=0;
+                                        //i=580;
+                                        /*if (a=='/')
                                         {
                                             znak=a;
                                             x=1;
                                             sum+=b;
                                             b=0;
-                                        }
+                                        }*/
                                         /*if (a>='0'&&a<='9')
                                         {
                                             b*=x;
@@ -256,8 +223,64 @@ int main()
                                             x=10;
                                         }*/
                                         }
-                                    else { i=-10;
-                                    if (znak=='+')
+                                    else
+                                    {
+                                        j=580;
+                                        while (j>0)
+                                        {
+                                            a=*(forresult+j);
+                                            if (a!=',')
+                                            {
+                                               // a=*(forresult+i);
+                                                //x=1;
+                                                
+                                                    while (j>0)
+                                                    {
+                                                         a=*(forresult+j);
+                                                         j++;
+                                                        if (a>='0'&&a<='9')
+                                                        {
+                                                   
+                                                    b*=x;
+                                                    b+=(float)(a-'0');
+                                                    x=10;
+                                                    }
+                                                       
+                                                        else
+                                                        {
+                                                            i=j;
+                                                            break;
+                                                        }
+                                                        
+                                                }
+                                               
+                                                if (a=='+')
+                                                {
+                                                    znak=a;
+                                                    sum+=b;
+                                                    b=0;
+                                                }
+                                               else if (znak=='+')
+                                                {
+                                                    sum+=b;
+                                                    b=0;
+                                                    znak='0';
+                                                }
+                                            }
+                                            else
+                                            {
+                                                std::cout <<sum<< std::endl;
+                                               // sleep(3);
+                                                j=-10;
+                                            }
+                                            
+                                        }
+                                        j=-10;
+                                        i=-10;
+                                    }
+                                    br=0;
+                                   // i=580;
+                                   /* if (znak=='+')
                                     {
                                         sum+=b;
                                         znak='0';
@@ -286,8 +309,11 @@ int main()
                                             
                                         }
                                         b=0;
-                                        x=1;}}
-                                        while ((int)(sum/x)>=1)
+                                        x=1;}*/
+                                    
+                                }
+                                x=1;
+                                       while ((int)(sum/x)>=1)
                                         {
                                             x*=10;
                                             cntn++;
@@ -345,6 +371,10 @@ int main()
                // *(sender+157)=sum;
                 free(forsnd);
                 free(math);
+                //free(forresult);
+                i=0;
+                j=0;
+                 std::cout <<"finish"<< std::endl;
                 //sum=0;
             }
             }
