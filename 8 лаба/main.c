52933a0f10170f8fedcdb57982e61fd8b26d7f18
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-09-23 00:10:30 +0300

    Есть указатели, удаляет Ava, но не работает с не одним словом

--- 8 лаба/main.c
+++ 8 лаба/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #define YES 1
 #define NO 0
-int bukwa(char c);
-int razd(char c);
-int glassniy(char c);
+int bukwa(char *buf);
+int razd(char *buf);
+int glassniy(char *buf);
 void process_line( char buffer[] );
 int main( void )
 {
@@ -15,6 +15,7 @@ int main( void )
 }
 void process_line( char buffer[] )
 {
+   
     char c;
     int buk=YES;
     int flag = NO;
@@ -24,58 +25,63 @@ void process_line( char buffer[] )
     int pos=0;
     int start=0;
     int j=0;
+    char *buf=buffer;
+     char buff;
     do
     {
         //test
         c = buffer[i];
-        if( razd(c)==1)
+        if( razd(buf)==1)
         {
             if( flag == YES )
             {
                 if( found == NO ||buk==NO ||a==0)
                 {
                     for( j = start; j < i; j++ )
+                      
                         buffer[pos++] = buffer[j];
                 }
             }
             a=0;
             buk=YES;
             flag = NO;
-            buffer[pos++] = c;
+            *buf++;  //test: *buf+=pos
+            buffer[pos++] = *buf;
         }
         else {
-            if (bukwa(c)==0)
+            if (bukwa(buf)==0)
                 buk=NO;
             if( flag == NO )
                 start = i;
             // запомнить позицию
-            if (glassniy(c)==1)
+            if (glassniy(buf)==1)
                 a++;
             if( a%2==0||a==0)
                 found = YES;
             else
                 found = NO;
             flag = YES; }
-        i++;
+        buff=*buf;
+        *buf++;
     }
-    while( c != '\0' );
+    while(  buff!= '\0' );
 }
-int razd(char c)
+int razd(char *buf)
 {
-    if (( c == ' ' || c == '.' || c == ',' ||
-         c == '\n' || c == '\0'))
+    if (( *buf == ' ' || *buf == '.' || *buf == ',' ||
+         *buf == '\n' || *buf == '\0'))
         return 1;
     return 0;
 }
-int glassniy(char c)
+int glassniy(char *buf)
 {
-    if ((c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='Y'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='y'))
+    if ((*buf=='A'||*buf=='E'||*buf=='I'||*buf=='O'||*buf=='U'||*buf=='Y'||*buf=='a'||*buf=='e'||*buf=='i'||*buf=='o'||*buf=='u'||*buf=='y'))
         return 1;
     return 0;
 }
-int bukwa(char c)
+int bukwa(char *buf)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    if ((*buf >= 'a' && *buf <= 'z') || (*buf >= 'A' && *buf <= 'Z'))
         return 1;
     return 0;
 }
