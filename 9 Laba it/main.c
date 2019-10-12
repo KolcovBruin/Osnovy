9801163127964ce843932487b8c7fccdd2acd278
GR <grisha25062@iclou.dom>
2019-10-12 13:11:31 +0300

    Тест методички

--- 9 Laba it/main.c
+++ 9 Laba it/main.c
@@ -1,15 +1,40 @@
-//
-//  main.c
-//  9 Laba it
-//
-//  Created by Григорий on 12.10.2019.
-//  Copyright © 2019 Григорий. All rights reserved.
-//
-
 #include <stdio.h>
+#define MAXLINE 1024
+int main( void )
+{
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+FILE *fpin;
+FILE *fpout;
+char line[MAXLINE];
+   char *ptr;
+fpin = fopen( "test.txt", "rt" );
+    if( fpin == NULL )
+return 0;
+
+fpout = fopen("test.txt", "wt" );
+    if( fpout == NULL )
+return 0;
+while( !feof( fpin ) )
+{
+ptr = fgets( line, MAXLINE, fpin );
+if( ptr == NULL )
+break;
+while( *ptr != '\0' )
+{
+if( *ptr == ' ' || *ptr == '.' || *ptr == ',' )
+{
+
+*ptr++ = '\n';
+
+*ptr = '\0';
+
+break; }
+ptr++;
+    
+}
+fputs( line, fpout );
+}
+fclose( fpin );
+    fclose( fpout );
+
 }
