f64dfeeedfa06d167a6a43b2b1c256ba337a7a77
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-10-14 00:23:52 +0300

    Считает число слов в самой длинной строке

--- 9 Laba it/main.c
+++ 9 Laba it/main.c
@@ -3,9 +3,11 @@
 #define Location "/Users/my/Desktop/проекты языка си/9 Laba it/9 Laba it/Data"
 int main( void )
 {
-
+    int cnt=0;// счётчик слов
+    int max_cnt=0;//число слов в самой длинной строке
+    int n_str=0;//номер строки в которой определили число слов
 FILE *fpin;
-FILE *fpout;
+//FILE *fpout;
 char line[MAXLINE];
    char *ptr;
 fpin = fopen( Location, "rt" );
@@ -18,28 +20,30 @@ return 0;
 return 0;*/
 while( !feof( fpin ) )
 {
-ptr = fgets( line, MAXLINE, fpin );
+ptr = fgets( line, MAXLINE, fpin );//??
 if( ptr == NULL )
 break;
 while( *ptr != '\0' )
 {
-if( *ptr == ' ' || *ptr == '.' || *ptr == ',' )
+if(( *ptr == ' ' || *ptr == '.' || *ptr == ','||*ptr=='\n'))//добавить условие для повторения разделителей)
 {
-
-*ptr++ = '\n';
-
-*ptr = '\0';
-
-break; }
+    cnt++;
+}
 ptr++;
     
 }
+    (cnt>max_cnt)?(max_cnt=cnt):(cnt=cnt);
+    cnt=0;
+    n_str++;
+    
+    //cnt++;
     /*fpout = fopen(Location, "wt" );
     if( fpout == NULL )
         return 0;
 fputs( line, fpout );*/
  //   ptr = fgets( line, MAXLINE, fpin );
 }
+    printf ("%i\n", max_cnt);
 fclose( fpin );
  //   fclose( fpout );
 
