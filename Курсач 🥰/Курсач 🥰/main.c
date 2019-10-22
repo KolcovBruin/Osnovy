a65f6d7ac2e7d769f891920186f6e3ba2c5303a3
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-10-23 00:09:08 +0300

    Тещу что-то из инета

---
+++ Курсач 🥰/Курсач 🥰/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+main () {
+    FILE *file;
+    struct food {
+        char name[20];
+        unsigned qty;
+        float price;
+    };
+    struct food shop[10];
+    char i=0;
+ 
+    file = fopen("fscanf.txt", "r");
+ 
+    while (fscanf (file, "%s%u%f", shop[i].name, &(shop[i].qty), &(shop[i].price)) != EOF) {
+        printf("%s %u %.2f\n", shop[i].name, shop[i].qty, shop[i].price);
+        i++;
+    }
+}
