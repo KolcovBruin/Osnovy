1614b5a66b5b21e5b0254141dbcb35e18f7a5c09
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-12-10 13:07:57 +0300

    В отчет

--- 4 ява/main.cpp
+++ 4 ява/main.cpp
@@ -86,7 +86,7 @@ int main(int argc, const char * argv[])
 //    }
     while (menu_1!=2)
     {
-    cout<<"Меню о продолжении работы"<<endl;
+    cout<<"Меню:\n  1:Ввести еще одну квартиру \n  2:Перейти в меню поиска"<<endl;
     cin>>menu_1;
     
     switch (menu_1) {
@@ -100,14 +100,16 @@ int main(int argc, const char * argv[])
     }
     int menu_2=0;
     int N;
+    int F_1; //для этажей
+    int F_2;
     while (menu_2!=5)
     {
-        cout<<"Меню о выборе функции"<<endl;
+        cout<<"Меню:\n    1:Поиск квартир по заданному кол-ву комнат\n    2:Поиск квартир по заданному кол-ву комнат и расположенных между заданными этажами\n    3:Поиск квартир больше заданной площади\n    5:выход "<<endl;
         cin>>menu_2;
         
         switch (menu_2) {
             case 1:
-                 cout<<"Поиск квартир по заданному кол-ву комнат\n"<<"Введите необзодимое число комнат"<<endl;
+                 cout<<"Поиск квартир по заданному кол-ву комнат\n"<<"Введите необходимое число комнат"<<endl;
                 cin>>N;
                 for(int j=0;j<cnt;j++)
                 {
@@ -118,24 +120,42 @@ int main(int argc, const char * argv[])
                      cout<<endl;
                  }
                 }
-//                 Work.set();
-//                    Home.push_back(Work);
-//                cnt++;
                 break;
                 
                 
                 case 2:
-                                Work.set();
-                                   Home.push_back(Work);
-                               cnt++;
-                               break;
+                                cout<<"Поиск квартир по заданному кол-ву комнат и расположенных между заданными этажами\n"<<"Введите необходимое число комнат"<<endl;
+                                               cin>>N;
+                cout<<"Введите мин этаж"<<endl;
                 
+                                                cin>>F_1;
+                 cout<<"Введите макс этаж"<<endl;
+                                                cin>>F_2;
+                                               for(int j=0;j<cnt;j++)
+                                               {
+                                                   Work=Home[j];
+                                                if  ( Work.get_Number_of_rooms()==N&&Work.get_Floor()<F_2&&Work.get_Floor()>F_1)
+                                                {
+                                                    Work.get();
+                                                    cout<<endl;
+                                                }
+                                               }
+                                               break;
                 
                 case 3:
-                                Work.set();
-                                   Home.push_back(Work);
-                               cnt++;
-                               break;
+                                 cout<<"Поиск квартир больше заданной площади\n"<<"Введите необходимую площадь"<<endl;
+                                               cin>>N;
+                cout<<"Квартиры превосходящие заданную площадь"<<endl;
+                                               for(int j=0;j<cnt;j++)
+                                               {
+                                                   Work=Home[j];
+                                                if  ( Work.get_Area()>N)
+                                                {
+                                                    Work.get();
+                                                    cout<<endl;
+                                                }
+                                               }
+                                               break;
         }
     }
     cout<<"конец программы"<<endl;
